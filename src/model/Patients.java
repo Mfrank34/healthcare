8104f1a795a2133a938cb24a568a6ec8ce8e9867
Michael Franks <mf23acb@herts.ac.uk>
2025-11-17 11:20:09 +0000

    adding other files and class files

--- src/model/Patients.java
+++ src/model/Patients.java
@@ -1,5 +1,105 @@
 package model;
 
-public class Patients {
+public class Patients extends Person {
+    private int nhs_number;
+    private char gender;
+    private String birth;
+    private String address;
+    private String postCode;
+    private String emergencyNumber;
+    private String registration;
+    private String gpId;
+
+    public Patients() {
+    }
+
+    public Patients(String id, String first, String last, String phone, String email, int nhs_number, char gender,
+            String birth, String address, String postCode, String emergencyNumber, String registration, String gpId,
+            String emergencyName) {
+        super(id, first, last, phone, email);
+        this.nhs_number = nhs_number;
+        this.gender = gender;
+        this.birth = birth;
+        this.address = address;
+        this.postCode = postCode;
+        this.emergencyNumber = emergencyNumber;
+        this.registration = registration;
+        this.gpId = gpId;
+        this.emergencyName = emergencyName;
+    }
+
+    public int getNhs_number() {
+        return nhs_number;
+    }
+
+    public void setNhs_number(int nhs_number) {
+        this.nhs_number = nhs_number;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    private String emergencyName;
+
+    public String getEmergencyName() {
+        return emergencyName;
+    }
+
+    public void setEmergencyName(String emergencyName) {
+        this.emergencyName = emergencyName;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getGpId() {
+        return gpId;
+    }
+
+    public void setGpId(String gpId) {
+        this.gpId = gpId;
+    }
+
+    public String getEmergencyNumber() {
+        return emergencyNumber;
+    }
+
+    public void setEmergencyNumber(String emergencyNumber) {
+        this.emergencyNumber = emergencyNumber;
+    }
 
 }
---
+++ src/model/Prescription.java
@@ -0,0 +1,103 @@
+package model;
+
+public class Prescription {
+    private String date;
+    private String drug;
+    private int dosage;
+    private String frequency;
+    private String duration;
+    private String quantity;
+    private String instructions;
+    private String pharmacy;
+    private String status;
+    private String issued;
+    private String collection;
+
+    public String getIssued() {
+        return issued;
+    }
+
+    public void setIssued(String issued) {
+        this.issued = issued;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(int dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getDrug() {
+        return drug;
+    }
+
+    public void setDrug(String drug) {
+        this.drug = drug;
+    }
+
+    public String getPharmacy() {
+        return pharmacy;
+    }
+
+    public void setPharmacy(String pharmacy) {
+        this.pharmacy = pharmacy;
+    }
+}
