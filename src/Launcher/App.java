b188c22c331c0fafa476ff86fe7dfb7b786d81df
Michael Franks <mf23acb@herts.ac.uk>
2025-12-04 16:48:44 +0000

    Rebasing to master combining filemanager with master

--- src/Launcher/App.java
+++ src/Launcher/App.java
@@ -1,20 +1,37 @@
 package Launcher;
 
-import Controllers.PatientsController;
-import Controllers.CliniciansController;
-import Model.CliniciansForm;
-import Views.PatientsForm;
-import Model.Patients;
-import Views.StaffForm;
+import javax.swing.*;
+
+import Controllers.HealthCareController;
+import Model.HealthCare;
+import Views.HealthCareGUI;
 
 public class App {
     public static void main(String[] args) {
-        Patients model = new Patients();
-        PatientsForm view = new PatientsForm();
-        new PatientsController(view, model);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                initializeApplication();
+            }
+        });
+    }
 
-        CliniciansForm model2 = new CliniciansForm();
-        StaffForm view2 = new StaffForm();
-        new CliniciansController(view2, model2);
+    private static void initializeApplication() {
+        System.out.println("Health Care system Starting...");
+
+        // generate model
+        HealthCare model = new HealthCare();
+        System.out.println("Generated HealthCare model...");
+
+        // generate view
+        HealthCareGUI view = new HealthCareGUI();
+        System.out.println("Generated HealthCare View...");
+
+        // Create Constructor for project
+        HealthCareController controller = new HealthCareController(model, view);
+        System.out.println("Created HealthCare Controller...");
+
+        // Set controller into view
+        view.setVisible(true);
+        System.out.println("application Running...");
     }
 }
